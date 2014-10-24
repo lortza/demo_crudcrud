bed0902ede586c2aa1dc9d5585c615128615ba65
Erik Trautman <erik.trautman@gmail.com>
2014-10-23 22:49:09 -0700

    try to rely on heroku_secrets...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  # config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   config.paperclip_defaults = {
     :storage => :s3,
