0706dd6a2e5d660fee86b78513744d47c80558ef
Erik Trautman <erik.trautman@gmail.com>
2014-10-23 23:05:12 -0700

    asdfasf u cksf asdgf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   puts "\n\n\n\n\n\n\n"
-  puts self.inspect
+  puts @secrets
   puts "\n\n\n\n\n\n\n"
   # config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
