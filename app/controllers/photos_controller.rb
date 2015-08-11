a0888ce580715bd6386a96761819358745788d8c
Erik Trautman <erik.trautman@gmail.com>
2015-08-11 10:21:18 -0700

    refactor photo uploads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,18 +14,34 @@ class PhotosController < ApplicationController
   end
 
   def create
-    # build a photo object with `new` and any non-data
-    # attributes that have been passed in
-    # then pass it into the block to set the
-    # additional data-based attributes if they're present
-    # Use model validations to verify this on save!
-    @photo = Photo.new(photo_params.except(:data)) do |t|
-      if photo_params[:data]
-        t.data      = photo_params[:data].read
-        t.filename  = photo_params[:data].original_filename
-        t.mime_type = photo_params[:data].content_type
-      end
-    end
+
+    # OPTION 1: The Controller Way
+    # 
+    # This option does all the work in the controller.
+    # 
+    # We build a photo object by passing `new` our block 
+    #   but only after stripping out the `data` param first.
+    #   The block then takes care of handling the data.
+    #
+    # @photo = Photo.new(photo_params.except(:photo_data)) do |t|
+    #   if photo_params[:photo_data]
+    #     t.data      = photo_params[:photo_data].read
+    #     t.filename  = photo_params[:photo_data].original_filename
+    #     t.mime_type = photo_params[:photo_data].content_type
+    #   end
+    # end
+
+
+    # OPTION 2: The Virtual Attribute Way
+    #
+    # This option relies on your model to have a 
+    #   virtual setter which will handle processing
+    #   the data from params.
+    # The good thing about this approach is that it
+    #  "just works" from the controller's perspective
+    #  since you can just use `Photo.new` as normal
+    # Just remember to permit your virtual attribute param!
+    @photo = Photo.new(photo_params)
 
     if @photo.save
       redirect_to(@photo, :notice => 'Photo was successfully created.')
@@ -36,7 +52,7 @@ class PhotosController < ApplicationController
 
   private
   def photo_params
-    params.require(:photo).permit(:data)
+    params.require(:photo).permit(:photo_data)
   end
 
 end
